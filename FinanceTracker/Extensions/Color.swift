88519474097c4be9aeed30c0e8dc249d859c92bc
DenisShabanov <Shabik35@yandex.ru>
2026-03-06 21:31:05 +0300

    [Feature] Created an extension for Colour and added colours for the application in assets

---
+++ FinanceTracker/Extensions/Color.swift
@@ -0,0 +1,28 @@
+//
+//  Color.swift
+//  FinanceTracker
+//
+//  Created by Denis Shabanov on 06.03.2026.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentAppColor")
+    let background = Color("")
+    let foodAndDrink = Color("")
+    let health = Color("")
+    let housing = Color("")
+    let other = Color("")
+    let shopping = Color("")
+    let transport = Color("")
+    
+}
