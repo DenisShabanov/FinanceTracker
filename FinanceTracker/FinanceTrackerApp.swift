50aefe02bd75babd6566249916da05045879ce5d
DenisShabanov <Shabik35@yandex.ru>
2026-03-06 20:31:46 +0300

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Denis Shabanov on 06.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinanceTrackerTests/FinanceTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerTests.swift
+//  FinanceTrackerTests
+//
+//  Created by Denis Shabanov on 06.03.2026.
+//
+
+import Testing
+@testable import FinanceTracker
+
+struct FinanceTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
