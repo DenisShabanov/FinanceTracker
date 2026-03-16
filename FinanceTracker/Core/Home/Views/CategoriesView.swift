94382626b0d4b6fab7ba8550d0151fe6e3aabbbf
DenisShabanov <Shabik35@yandex.ru>
2026-03-16 17:58:15 +0300

    [Features] Create CategoriesView

---
+++ FinanceTracker/Core/Home/Views/CategoriesView.swift
@@ -0,0 +1,25 @@
+//
+//  CategoriesView.swift
+//  FinanceTracker
+//
+//  Created by Denis Shabanov on 16.03.2026.
+//
+
+import SwiftUI
+
+struct CategoriesView: View {
+    
+    //MARK: - Body
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(L10n.HomeView.Categories.title)
+        }
+    }
+}
+
+//MARK: - Preview
+
+#Preview {
+    CategoriesView()
+}
